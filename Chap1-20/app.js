d43eb6c17a0101ef05d05a023bc6097a85fc8d3d
aasmatalib <aasmatalib@gmail.com>
2023-09-15 23:25:04 +0500

    Update app.js

--- Chap1-20/app.js
+++ Chap1-20/app.js
@@ -344,8 +344,446 @@ console.log(x);
 /* Chapter 11 (Comparison Operators)*/
 
 /* 1 . Code the first line of an if statement that tests whether one variable is unequal to another. (Use !) 
-if(x !==y){
+ Answer : if(x !==y){
 
     */
 
+/* 2.	Code the first line of an if statement that tests whether the value represented
+ by a variable is greater than or equal to the value represented by another variable.*/
+
+
+/*3.	Code an if statement. Test whether a variable is unequal to a particular number. 
+If so, assign a number to that variable.
+
+var a;
+var a = prompt("Enter a number");
+if(a!==10){
+a = 10 ;
+console.log(a);
+} */
+
+/*4.	Code an if statement that tests whether a number is unequal to a different number. If the condition is true
+ (it will be), display a congratulations alert.
+
+ var a = prompt("enter first number");
+ var b = prompt("enter second number");
+  if(a!==b){
+ alert("congradulations");
+ 
+  } */
+
+  /*5.	Code a prompt asking for your first name.
+Code an if statement that tests whether the name you entered is unequal to another name.
+If the condition is true (it will be), display an alert that says "No match"
+
+var username = prompt("Enter username");
+if(username !== "admin"){
+alert("No match ");
+
+} */
+
+/*Chapter 12 (if…else and else if statements)*/
+/*1.	Code an if statement that tests whether the value represented 
+by a variable is greater than or equal to the value represented by another variable. If so, display an alert. If not, display a different alert.
+var a = prompt("enter first number");
+ var b = prompt("enter second number");
+  if(a!==b){
+ alert("congradulations");
+ 
+  }else 
+  alert("Enter roll number ");
+  */
+
+/*2.	Write a program using if else and else if statement which take marks from user. 
+ the program will calculate your percentage and  give you grade A/C to Your percentage. (MARKSHEET)
+
+ var marks = prompt("ENter your Marks");
+ if(marks >= 80 && marks <=100) 
+ {
+    alert("A Grade");
+ }else if (marks >= 70 && marks <= 80)
+ {
+    alert("B Grade");
+ }else if (marks >= 60 && marks <= 70)
+ {
+    alert("C Grade");
+ } */
+
+/*3.	This is the if statement that begins the code.
+if (a === 10) {
+  alert("a is 10");
+}
+If a isn't 10, display an alert that says The correct value of a is 
+var a = parseInt(prompt("Enter value for a"));
+if (a === 10) {
+    alert("a is 10");
+  }else alert("The correct value of a is ___________"); */
+
+  /*4.	Prompt the user to enter a city.
+If the city is Karachi, display an alert acknowledging it is Karachi.
+If not, check to see if it's Lahore.
+If it is, display an alert acknowledging it's Lahore.
+Otherwise, display a different alert.
+
+var city = prompt("enter your city ");
+var input = city.toUpperCase();
+if(input === "KARACHI"){
+alert("Your city is Karachi");
+
+}else if(input === "LAHORE"){
+
+    alert("your city is Lahore");
+
+}
+else
+{
+    alert("whats your city then ?");
+}
+*/
+
+/*Chapter 13 (Testing sets of conditions)
+
+1.	Code the first line of an if statement that tests whether both are true: a has the same value as b and c has the same value as d.
+if(a===b && c===d)
+*/
+/*2.	Code the first line of an if statement that tests whether either or both are true: a has the same value as b or c doesn't have the same value as d.*/
+
+/*3.	Code the first line of an if statement that tests whether 
+I.	name is either "Hamza" or "Arsalan".
+II.	age is Less than 60.
+if((Name === "Hamza" || Name === "Arsalan") && (age <= 60)){
+    */
+   /*4.	Declare two variables and assign them number values.
+If the first variable is less than the second variable or greater than the second variable, display an alert.
+
+var a = prompt("Enter first number ");
+var b = prompt("Enter second number");
+
+if((a < b) || (a > b)){
+    alert("Not equal numbers");}
+
+else {alert("You enetered Equal Numbers")};
+*/
+/*5.	Declare 2 variables. Assign one of them your first name and the other one your last name.
+Code 2 prompts, asking for your first and your last name.
+If your answers match the two variables, display an alert.
+var fName = prompt("ENter your firts name");
+var lName = prompt("ENter you last name");
+
+if(fName === "asma") && (lName === "talib"){
+    alert("you entered your name");
+}
+*/
+
+
+/*===================================
+Chapter 14 (If statements nested)
+=====================================*/
+
+/*1.	Code an if statement enclosing a nested if. If password is not empty, then check if password is not greater than 5  
+, then display an alert that says "Password must be greater than 5" if greater than 5 then Alert "OK".
+
+
+var input = prompt("Enter password");
+var len = input.length; // Use .length to get the length of a string
+console.log(len);
+
+if (len !== null) { // Corrected the condition
+    if (len >= 5) { // Corrected the condition and added a missing closing brace
+        // Your code for a password length of at least 5 characters goes here
+    } else {
+        console.log("Password must be at least 5 characters long.");
+    }
+} else {
+    console.log("You didn't enter a password.");
+}
+} */
+
+/*2.	Try this statement by yourself
+if (a === 1) {
+ if (c === "Max") {
+    alert("OK");
+  }
+}
+
+a and c are not defined output -> variable undefined */
+
+/*3.	Code the first line of an if statement that avoids the nesting above by testing for multiple conditions.
+if (a === 1) {
+  if (c === "Max") {
+ alert("OK");
+  }
+}*/
+
+/*4.	Declare two variables and assign them the same number value.
+Test two conditions, using nested if statements. Test whether the first variable
+ equals the second variable and also whether it is less than or equal to the second variable.
+  If the test passes—and it will—display an alert message.
+
+
+var a = 10 ;
+var b = 12;
+if((a<b) || (a>b)){
+alert("not equal");
+
+}else {
+    alert("Equal");}*/
+
+   /*==================================
+    Chapter 15 (Array I)
+    ===================================
+    */
+
+    /* 1.	Declare an empty array.
+    var names =[]'
+    */
+/*2.	Code an array with 1 string element
+var names = ['asma'];
+
+*/
+/*3.	var alphabet = ["h","i","j","k"]. Now print the letter “j” in alert using array index 
+var alphabet = ["h","i","j","k"];
+alert("alphabet[2]");*/
+
+/*4.	var alphabet=["h","i","j","k", “l”,”m”, “n”, “o”]. Find the total length of array.
+var alphabet = ["h","i","j","k" , "h","i","j","k"];
+var len = alphabet.length;
+alert(len); */
+
+/*5.	Declare an array with one element and Add a second element with index in array.
+Create an alert whose message is the new element.
+var fName = ['asma'];
+fName.push('myname');
+var index = fName.indexOf(fName[1]);
+alert(index);
+
+*/
+/*Chapter 16 (Array II)*/
+/*
+1.	Code an array with 1 string element.
+Add an additional element to the array using push.
+Create an alert whose message is the last element.
+
+var fName = ['asma'];
+fName.push('myname');
+var index = fName.indexOf(fName[1]);
+alert(fName[index +1]);
+*/
+/* 2.	var Alphabet=["h","i","j","k"]
+Remove the last element from the array Alphabet.
+
+var Alphabet = ["h","i","j","k"];
+var text = Alphabet.pop();*/
+
+/*3.	var Alphabet=["h","i","j","k"]
+Add a new element, a number, to the end of an array.
+
+var Alphabet=["h","i","j","k"];
+Alphabet.push("l");
+console.log(Alphabet);
+*/
+
+/*3.	var Alphabet=["h","i","j","k"]
+Add a new element, a number, to the end of an array.
+Chapter 16 (Array III)
+*/
+/* 1.	var sizes = ["S", "M", "XL", "XXL", "XXXL"].
+Remove the first element of an array.
+var myArray = [1, 2, 3, 4, 5]; 
+myArray.shift(); 
+console.log(myArray); 
+*/
+
+/*2.	var sizes = ["S", "M", "XL", "XXL", "XXXL"].
+Add three number elements to the beginning of an array.
+
+var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+sizes.unshift(1,2,3);
+console.log(sizes);
+*/
+
+/* 3.	Declare an array with one element.
+Add a second element to the beginning of the array.
+Create an alert whose message is the new first element.
+var arr= [1];
+arr.unshift("2");
+alert(arr[0]);*/
+
+/*
+
+4.	var sizes = ["S", "M", "XL", "XXL", "XXXL"].
+Insert "L" into the array between "M" and "XL".
+
+var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+sizes.splice(2,0, "L");
+console.log(sizes);
+
+*/
+
+/*5.	var sizes = ["S", "M", "XL", "XXL", "XXXL"].
+Copy the first 3 sizes of the array and put them into a new array, regSizes.
+
+var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+var newSizes = sizes.splice(3,0);
+console.log(newSizes);*/
+
+/*6.	var pets = ["dog", "cat", "ox", "duck", "frog"].
+Add 2 elements after "dog" and remove "cat", "ox", and "duck".
+
+var pets = ["dog", "cat", "ox", "duck", "frog"];
+
+pets.splice(1,2);
+pets.splice(1,0,"monkey" , "donkey" , "bear");
+console.log(pets);
+*/
+/* 7.	var pets = ["dog", "cat", "ox", "duck", "frog"];
+Remove "cat" and "ox".
+
+var pets = ["dog", "cat", "ox", "duck", "frog"];
+pets.splice(1,2);
+console.log(pets);
+*/
+/*8.	var pets = ["dog", "cat", "ox", "duck", "frog", "flea"];
+Reduce it to "duck" and "frog" using slice.
+var pets = ["dog", "cat", "ox", "duck", "frog", "flea"]
+pets.splice(0,3);
+console.log(pets);*/
+
+/*Chapter 17 - 20 (for Loops)*/
+
+/* 1.	Write a statement in which loop is to run 10 times. 
+var a;
+for(a=0 ;a< 10 ;a++) {
+    console.log("print no" + (a+1) );
+}*/
+/*2.	Code the first line of a for loop with the usual counter name, usual starting value,
+ and usual increment. Run it 12 times using <= to specify how many loops.
+
+ for(var  i =0 ; i< 13 ; i++){
+    console.log("print no" + (i) );
+
+}
+*/
+/* 3.	What are the 5 characters missing from this code, excluding any spaces that are missing? 
+Type them in order, with no spaces or commas between them.
+for( var i = 0 ; i <= 4 ; i++)*/
+/*4.	Code the first line of a for loop with a counter name that's not i.
+Code the usual starting value and usual increment. Run it 100 times using < to specify how many loops.
+
+for(var a = 0 ; a<100 ; a++){
+
+    console.log("print no" + (a+1) );*/
+
+}
+
+/*5.	Code the first line of a for loop with the usual counter and the usual starting value. 
+Run it 3 times using > to specify how many loops. Decrement it with each iteration.
+for(var i = 3 ; i>0 ; i--){
+
+    console.log("print no" + i );
+
+}*/
+
+/*6.	The statement assigns the number of elements in the array to the variable
+var arr = [1,2,3,4,5,6,7];
+var b = arr.length;
+console.log(b); */
+/* 7.	Set a variable named “flag” with an initial Boolean value of your choice.
+
+var flag = true;*/
+
+/*8.	Code the first line of a for loop with the usual counter, the usual starting value, and the usual incrementing.
+ Limit the number of loops by the number of elements in the array pets.
+
+ var pets = [ "cat", "dog" , "monkey", "bear", "panda"];
+ var len = pets.length;
+ for(var a =0 ;a<len; a++){
+    console.log(pets[a]);*/
+
+    /*9.	Coding Exercise:
+Set a for loop to run 10 iterations.
+On the second iteration, display the counter in an alert. (It should be 1.)
+Break out of the loop.
+for(var a = 0 ; a< 11 ;a++){
+    console.log(a);
     
+if(a == 1) break;
+}
+*/
+
+/*10.	Create an array which contains user names
+Code a prompt with the message "Enter first name". The user's response is assigned to firstName.
+Code the first line of a for loop with the usual counter, the usual starting value, and the usual incrementing. Limit the number of loops by 
+the number of elements in the array user names.
+Code an if statement that tests the presense of (user name) in an array.
+
+var firstName = prompt( "Enter first name");
+console.log(firstName);
+let len = firstName.length;
+console.log(len);
+
+var userName = ["tom" , "jerry" , "bear" , "panda" , "monkey"];
+var fName = prompt("Enter first name");
+
+for(var i = 0 ; i< userName.length ; i++){
+if(fName === userName[i]){
+alert("Welcome " + userName[i]);
+}
+*/
+
+/*11.	Complete this code to display an alert if a match isn't found.
+var matchFound = false;
+for (var i = 0; i < list.length; i++) {
+  if (userInput === list[i]) {
+    alert("Match found");
+    matchFound = true;
+    break;
+  }
+};
+
+
+var userInput = +prompt("Enter a number");
+var list = [1, 2, 3, 4];
+var matchFound = false; // Initialize matchFound outside the loop
+
+for (var i = 0; i < list.length; i++) {
+  if (userInput === list[i]) {
+    matchFound = true;
+    break; // Exit the loop if a match is found
+  }
+}
+
+if (matchFound) {
+  alert("Match found");
+} else {
+  alert("No match found");
+}*/
+
+/*12.	var firstArr = [“a”, “b”, “c”, “d”, “e”, “f”];
+var secondArr = [1, 2, 3, 4, 5, 6];
+Code the first line of a for loop with the usual counter, the usual starting value, and the usual incrementing. Limit the number of loops by the number of elements in the array firstArr.
+In the scope of main loop Code the nested loop. Limit the number of nested loops by the number of elements in the array secondArr.
+After that concatenate the both loops.
+
+Expected Output:
+a1
+a2
+a3
+a4
+…
+f6
+
+var firstArr = [ "a", "b", "c", "d", "e", "f"];
+
+var secondArr = [1, 2, 3, 4, 5, 6];
+for(var i = 0 ;i< firstArr.length ; i++){
+for(var j  = 0 ; j < secondArr.length  ; j++){
+
+console.log(firstArr[i] + secondArr[j]);
+}
+
+}
+*/
+
+
+ 
